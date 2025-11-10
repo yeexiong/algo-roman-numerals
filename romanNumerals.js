d2bd0688500a2044fa1b285aceb16ed1b6500258
yeexiong <yee.l.xiong@gmail.com>
2025-11-10 17:49:22 -0600

    Homework 1

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,6 +1,38 @@
+let romanNumeralToArabic = {
+  M: 1000,
+  D: 500,
+  C: 100,
+  L: 50,
+  X: 10,
+  V: 5,
+  I: 1,
+};
+
 function toRomanLazy(num) {
-  return "";
+  const romanNumeralPriorityOrder = ["M", "D", "C", "L", "X", "V", "I"];
+
+  // build per-call; don't use a global
+  let output = "";
+  let difference = num;
+
+  for (let i = 0; i < romanNumeralPriorityOrder.length; i++) {
+    let romanLetter = romanNumeralPriorityOrder[i];
+    let arabicNumber = romanNumeralToArabic[romanLetter];
+
+    let quotient = Math.floor(difference / arabicNumber);
+
+    if (quotient > 0) {
+      output += romanLetter.repeat(quotient);
+      difference -= arabicNumber * quotient;
+    }
+
+    if (difference === 0) break;
+  }
+
+  return output;
 }
+
+console.log(toRomanLazy(5250));
 
 function toRoman(num) {
   return "";
